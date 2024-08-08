cdc068e818ef9e6919101f164eee761af4ede2cd
zombieghosts <cotebaez123@gmail.com>
2024-08-07 21:51:47 -0400

    Create main.py

---
+++ main.py
@@ -0,0 +1,14 @@
+meme_dict = {
+            "CRINGE": "Algo excepcionalmente raro o embarazoso",
+            "LOL": "Una respuesta común a algo gracioso",
+            "XD": "cara riendo",
+            ":v":"pacman",
+            ":3": "cara tierna o de gato"
+            }
+
+word = input("Escribe una palabra que no entiendas (¡con mayúsculas!): ")
+
+if word in meme_dict.keys():
+    print(meme_dict[word])
+else:
+    print("Esto no está en el diccionario")
